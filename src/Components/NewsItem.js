7303f2dc49b61517b3208c2a5fc1070e137f9f9b
ranjeetrock <ranjeetsahai3579@gmail.com>
2023-12-07 16:40:10 +0530

    Adding ReactJs NewsApp

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,35 @@
+import React  from 'react'
+
+const NewsItem  = (props)=> {
+  
+
+  
+    let {title, description, newsUrl,imageUrl,author,date,source} = props;
+    return (
+      <div className='my-3'> 
+        <div className="card" >
+          <div style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            position:'absolute',
+            right:'0'
+          }}>
+          <span className=" badge rounded-pill bg-danger " >
+    {source}
+    
+  </span>
+          </div>
+  <img src={!imageUrl?"https://images.moneycontrol.com/static-mcnews/2021/06/shutterstock_734792920-770x433.jpg":imageUrl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title"> {title} </h5>
+    <p className="card-text">{description}</p>
+    <p className="card-text"><small className="text-muted"> By {!author?"Uknown":author} on {new Date(date).toGMTString()}</small></p>
+    <a rel="noreferrer" href={newsUrl}  target='_blank'className="btn btn-sm btn-danger">Read More</a>
+  </div>
+</div>
+
+      </div>
+    )
+  }
+
+export default NewsItem
